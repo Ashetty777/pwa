836f691b61684a28747326a9e1a67603a12f32d6
Ashetty777 <ashrithshetty260@gmail.com>
2024-05-27 11:03:23 +0530

    screen

--- vite.config.js
+++ vite.config.js
@@ -23,9 +23,29 @@ export default defineConfig({
             purpose:"maskable",
           },
         ],
-      },
-      description:
+        description:
           'Pwa Setup Project Where you can send notifications and also click pictures record vedio and install the application in the mobile devices',
+        screenshots: [
+          {
+            src: 'pwa-192x192.png',
+            type: 'image/png',
+            sizes: '590x525',
+            form_factor: 'narrow',
+          },
+          {
+            src: 'pwa-192x192.png',
+            type: 'image/png',
+            sizes: '577x731',
+            form_factor: 'wide',
+          },
+          {
+            src: 'pwa-192x192.png',
+            type: 'image/png',
+            sizes: '603x713',
+            form_factor: 'wide',
+          },
+        ],
+      },
       registerType: 'autoUpdate',
       workbox: {
         clientsClaim: true,
